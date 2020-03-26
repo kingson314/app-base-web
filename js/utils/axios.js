34c434df092097b7971dcffc9e60d5e8ac33d1f0
guoqufeng <273394694@qq.com>
2020-03-26 14:45:45 +0800

    扩展axios

--- js/utils/axios.js
+++ js/utils/axios.js
@@ -4,24 +4,88 @@ var _axios = require('axios');
 
 var _axios2 = _interopRequireDefault(_axios);
 
+var _queryString = require('query-string');
+
+var _queryString2 = _interopRequireDefault(_queryString);
+
+var _net = require('./net');
+
+var _net2 = _interopRequireDefault(_net);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = {
+var _Export = {
     _DicList: {},
     _DicJson: {},
-    get: function get(url, data) {
-        return new Promise(function (resolve, reject) {
-            _axios2.default.get(url).then(function (response) {
-                resolve(response.data);
-            }, reject);
-        });
+    // get:function(url, data) {
+    //     return new Promise((resolve, reject) => {
+    //         axios.get(url)
+    //             .then((response) => {
+    //                 resolve(response.data)
+    //             }, reject)
+    //     })
+    // },
+    setUser: function setUser(user) {
+        if (!user) {
+            _net2.default.localStorage.remove("_token");
+            _net2.default.localStorage.remove("_userId");
+            _net2.default.localStorage.remove("_userName");
+            _net2.default.localStorage.remove("_userType");
+            _net2.default.localStorage.remove("_menuId");
+        } else {
+            _net2.default.localStorage.set("_token", user.token);
+            _net2.default.localStorage.set("_userId", user.id);
+            _net2.default.localStorage.set("_userName", user.name);
+            _net2.default.localStorage.set("_userType", user.type);
+            _net2.default.localStorage.set("_menuId", user.memo);
+        }
+    },
+    getUser: function getUser() {
+        var user = {
+            token: _net2.default.localStorage.get("_token") || "",
+            id: _net2.default.localStorage.get("_userId") || "",
+            name: _net2.default.localStorage.get("_userName") || "",
+            type: _net2.default.localStorage.get("_userType") || ""
+        };
+        return user;
     },
     post: function post(url, data, callback) {
+        var user = _Export.getUser();
+        var _log = JSON.stringify({
+            userId: String(user.id)
+        });
         return new Promise(function (resolve, reject) {
-            _axios2.default.post(url, data).then(function (response) {
+            _axios2.default.post(url, data, {
+                headers: {
+                    _log: _log,
+                    _langue: "zh_CN",
+                    _token: user.token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function (response) {
                 resolve(response.data);
                 callback && callback(response.data);
             }, reject);
         });
+    },
+
+    //global defaults
+    interceptors: function interceptors(f_req, f_res) {
+        _axios2.default.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        _axios2.default.interceptors.request.use(function (config) {
+            config.timeout = 6000;
+            // config.withCredentials = true; // 允许携带token ,这个是解决跨域产生的相关问题
+            config.data = _queryString2.default.stringify(config.data);
+            config = f_req(config);
+            return config;
+        }, function (error) {
+            return Promise.reject(error);
+        });
+        _axios2.default.interceptors.response.use(function (response) {
+            return f_res(response);
+        }, function (error) {
+            return Promise.reject(error);
+        });
     }
 };
+module.exports = _Export;
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,22 +1,78 @@
 import axios from 'axios'
-module.exports = {
-    _DicList:{},
-    _DicJson:{},
-    get:function(url, data) {
-        return new Promise((resolve, reject) => {
-            axios.get(url)
-                .then((response) => {
-                    resolve(response.data)
-                }, reject)
-        })
+import qs from 'query-string'
+import net from './net';
+const _Export = {
+    _DicList: {},
+    _DicJson: {},
+    // get:function(url, data) {
+    //     return new Promise((resolve, reject) => {
+    //         axios.get(url)
+    //             .then((response) => {
+    //                 resolve(response.data)
+    //             }, reject)
+    //     })
+    // },
+    setUser: function (user) {
+        if (!user) {
+            net.localStorage.remove("_token");
+            net.localStorage.remove("_userId");
+            net.localStorage.remove("_userName");
+            net.localStorage.remove("_userType");
+            net.localStorage.remove("_menuId");
+        } else {
+            net.localStorage.set("_token", user.token);
+            net.localStorage.set("_userId", user.id);
+            net.localStorage.set("_userName", user.name);
+            net.localStorage.set("_userType", user.type);
+            net.localStorage.set("_menuId", user.memo);
+        }
     },
-    post:function(url, data,callback) {
-        return new Promise((resolve, reject) => {
-            axios.post(url, data)
-                .then((response) => {
-                    resolve(response.data);
-                    callback&& callback(response.data);
-                }, reject)
+    getUser: function () {
+        let user = {
+            token: net.localStorage.get("_token") || "",
+            id: net.localStorage.get("_userId") || "",
+            name: net.localStorage.get("_userName") || "",
+            type: net.localStorage.get("_userType") || "",
+        };
+        return user;
+    },
+    post: function (url, data, callback) {
+        let user = _Export.getUser();
+        let _log = JSON.stringify({
+            userId: String(user.id),
         });
+        return new Promise((resolve, reject) => {
+            axios.post(url, data,{
+                headers: {
+                    _log,
+                    _langue: "zh_CN",
+                    _token: user.token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then((response) => {
+                resolve(response.data);
+                callback && callback(response.data);
+            }, reject);
+        });
+    },
+
+    //global defaults
+    interceptors: function (f_req, f_res) {
+        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+        axios.interceptors.request.use(config => {
+            config.timeout = 6000;
+            // config.withCredentials = true; // 允许携带token ,这个是解决跨域产生的相关问题
+            config.data = qs.stringify(config.data);
+            config=f_req(config)
+            return config;
+        }, error => {
+            return Promise.reject(error);
+        })
+        axios.interceptors.response.use(response => {
+           return  f_res(response);
+        }, error => {
+            return Promise.reject(error);
+        }); 
     }
-}      
+}
+module.exports = _Export;
